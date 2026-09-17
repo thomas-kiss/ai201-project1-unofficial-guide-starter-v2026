2c0190c969266197ffd93722625787a3747ba722
Thomas Alex Kiss <kiss.thomas@gmail.com>
2026-09-16 17:59:14 -0700

    initial commit

--- store.py
+++ store.py
@@ -63,7 +63,8 @@ class _OnnxEmbedder:
     def __init__(self):
         from chromadb.utils.embedding_functions import ONNXMiniLM_L6_V2
 
-        self._ef = ONNXMiniLM_L6_V2()
+        # Force CPU execution to bypass Apple CoreML runtime errors
+        self._ef = ONNXMiniLM_L6_V2(preferred_providers=["CPUExecutionProvider"])
 
     def encode(self, texts, show_progress_bar: bool = False):
         return [vector.tolist() for vector in self._ef(list(texts))]
